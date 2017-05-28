774f9b822731396e85b52684e0c24033289927db
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-28 02:21:04 -0600

    posicionando las fotos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 
 var miSeccion = document.getElementById("contenedor");
 
+/*var miDiv = document.createElement("div");
+miDiv.setAttribute("id","sub-contenedor");
+miSeccion.appendChild(miDiv);*/
+
 var arrayDivs = new Array(6)
 for (i=0 ; i<6 ; i++){
 	arrayDivs[i] = document.createElement("div");
 	arrayDivs[i].setAttribute("class","fotos");
 	arrayDivs[i].setAttribute("id","caja" + i.toString());
+
+	arrayDivs[i].style.position = "relative";
+	arrayDivs[i].style.backgroundSize = "cover";
+	arrayDivs[i].style.boxSizing = "border-box";
 
 	miSeccion.appendChild(arrayDivs[i]);
 }
